da7624f7e1a33175106e9e896bd3734ec1498329
JulietteCoemelck <juliette.coemelck@gmail.com>
2022-02-28 14:57:09 +0100

    deploy

--- reactapp/src/components/HomePage/HomePage.js
+++ reactapp/src/components/HomePage/HomePage.js
@@ -10,7 +10,7 @@ export default function HomePage(){
 
     // Requête au backend qui fait le call API //
     async function fetchData(input){
-        const normalizedInput = input.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // suppression des accents
+        //const normalizedInput = input.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // suppression des accents
         const response = await fetch('/companies', {
             method: 'POST',
             headers: {'Content-Type':'application/x-www-form-urlencoded'},
